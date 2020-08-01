c838bf5ad36113319c715f6b8d73c9d3fffe4210
David Seidenberg <davidseidenberg@Davids-MacBook-Pro.local>
2020-08-01 10:30:32 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity
+          style={styles.dingButton}
+          onPress={() => alert('You tapped the button!')}
+          underlayColor='#fff'>
+          <Text style={styles.dingText}>Ding</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +23,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dingButton:{
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    padding: 20,
+    backgroundColor:'#33FF99',
+    borderRadius:20,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  dingText:{
+      color:'#fff',
+      textAlign:'center',
+      paddingLeft : 10,
+      paddingRight : 10,
+      fontSize: 50,
+  }
 });
