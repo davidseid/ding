1c0d86b5ed58880e2b9129a67316197c0c9b2c81
davidseid <davidseidenberg@gmail.com>
2020-08-01 14:29:39 -0700

    Add popping cat animation occasionally

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import ConfettiCannon from 'react-native-confetti-cannon';
 import { Audio } from 'expo-av';
+import LottieView from 'lottie-react-native';
 
 const soundObject = new Audio.Sound();
 
@@ -10,9 +11,21 @@ const soundObject = new Audio.Sound();
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    
+    this.animations = [this.popCat];
+
+    for (let i = 0; i < 4; i++) {
+      this.animations.push(this.explodeConfetti);
+    }
+    
     this.explosion;
 
     this.loadSounds();
+
+    this.state = {
+      catIsPopping: false,
+    };
   }
 
   loadSounds = async () => {
@@ -23,12 +36,18 @@ export default class App extends Component {
     }
   }
 
-  // state={
-  //   exploded:false
-  // }
+  explodeConfetti = () => {
+    this.explosion && this.explosion.start();
+  }
 
+  popCat = () => {
+    this.setState({
+      catIsPopping: !this.state.catIsPopping,
+    });
+    this.cat && this.cat.play();
+  }
 
-  renderResults= async () =>{
+  renderResults = async () =>{
 
     try {
       await soundObject.replayAsync();
@@ -37,32 +56,37 @@ export default class App extends Component {
       // An error occurred!
     }
 
-    this.explosion && this.explosion.start();
+    let choice = Math.floor(Math.random() * 10) % this.animations.length;
 
-    // this.setState({
-    //   exploded:!this.state.exploded //toggles the visibilty of the text
-    // });
-
+    this.animations[choice]();
   } 
   render() {
     return (
       <View style={styles.container}>
-      <StatusBar style="auto" />
-      <TouchableOpacity
-          style={styles.dingButton}
-          onPress={this.renderResults}
-          underlayColor='#fff'>
-          <Text style={styles.dingText}>Ding!</Text>
-      </TouchableOpacity>
-      <ConfettiCannon 
-        count={70} 
-        origin={{x: -10, y: 0}}
-        explosionSpeed={400}
-        fallSpeed={3000}
-        autoStart={false} 
-        fadeOut={true} 
-        ref={ref => (this.explosion = ref)}
-      />
+        {this.state.catIsPopping?<LottieView 
+          source={require('./assets/cat_pop.json')}
+          ref={cat => {
+            this.cat = cat;
+          }}
+          loop={false}
+          onAnimationFinish={() => this.setState({ catIsPopping: false })}
+        />:null}
+        <StatusBar style="auto" />
+        {!this.state.catIsPopping?<TouchableOpacity
+            style={styles.dingButton}
+            onPress={this.renderResults}
+            underlayColor='#fff'>
+            <Text style={styles.dingText}>Ding!</Text>
+        </TouchableOpacity>:null}
+        <ConfettiCannon 
+          count={70} 
+          origin={{x: -10, y: 0}}
+          explosionSpeed={400}
+          fallSpeed={3000}
+          autoStart={false} 
+          fadeOut={true} 
+          ref={ref => (this.explosion = ref)}
+        />
       </View>
     );
   }
@@ -91,5 +115,5 @@ const styles = StyleSheet.create({
       paddingLeft : 10,
       paddingRight : 10,
       fontSize: 50,
-  }
+  },
 });
